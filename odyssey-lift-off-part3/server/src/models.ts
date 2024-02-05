4acfb2d4d8bd6b89de13b6b68785ff2ee9f13333
william-lim-wen-bin <132965638+william-lim-wen-bin@users.noreply.github.com>
2024-02-05 23:02:43 +0800

    [part 3] Updating schema and adding new query entry point

--- odyssey-lift-off-part3/server/src/models.ts
+++ odyssey-lift-off-part3/server/src/models.ts
@@ -5,10 +5,18 @@ export type TrackModel = {
   thumbnail: string;
   length: number;
   modulesCount: number;
+  description: string;
+  numberOfViews: number;
 };
 
 export type AuthorModel = {
   id: string;
   name: string;
   photo: string;
+};
+
+export type ModuleModel = {
+  id: string;
+  title: string;
+  length: number;
 };
