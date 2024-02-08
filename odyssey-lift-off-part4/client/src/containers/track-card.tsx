a53ccbf8ab5f53c54735282119810310ae9eb83e
william-lim-wen-bin <132965638+william-lim-wen-bin@users.noreply.github.com>
2024-02-08 17:21:08 +0800

    [part 4] Implement the FE

--- odyssey-lift-off-part4/client/src/containers/track-card.tsx
+++ odyssey-lift-off-part4/client/src/containers/track-card.tsx
@@ -4,6 +4,25 @@ import { colors, mq } from "../styles";
 import { humanReadableTimeFromSeconds } from "../utils/helpers";
 import { Link } from "react-router-dom";
 import type { Track } from "../__generated__/graphql";
+import { useMutation } from "@apollo/client";
+import { gql } from "../__generated__";
+
+/**
+ * Mutation to increment a track's number of views
+ */
+const INCREMENT_TRACK_VIEWS = gql(`
+  mutation IncrementTrackViews($incrementTrackViewsId: ID!) {
+    incrementTrackViews(id: $incrementTrackViewsId) {
+      code
+      success
+      message
+      track {
+        id
+        numberOfViews
+      }
+    }
+  }
+`);
 
 /**
  * Track Card component renders basic info in a card format
@@ -11,9 +30,16 @@ import type { Track } from "../__generated__/graphql";
  */
 const TrackCard: React.FC<{ track: Omit<Track, "modules"> }> = ({ track }) => {
   const { title, thumbnail, author, length, modulesCount, id } = track;
+  const [incrementTrackViews] = useMutation(INCREMENT_TRACK_VIEWS, {
+    variables: { incrementTrackViewsId: id },
+    // to observe what the mutation response returns
+    onCompleted: (data) => {
+      console.log({ data });
+    },
+  });
 
   return (
-    <CardContainer to={`/track/${id}`}>
+    <CardContainer to={`/track/${id}`} onClick={() => incrementTrackViews()}>
       <CardContent>
         <CardImageContainer>
           <CardImage src={thumbnail || ""} alt={title} />
