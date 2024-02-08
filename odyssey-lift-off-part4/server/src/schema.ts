36a17c007907970c7d59e9998c00896fda5a1cab
william-lim-wen-bin <132965638+william-lim-wen-bin@users.noreply.github.com>
2024-02-08 16:13:43 +0800

    [part 4] Implement the mutation schema

--- odyssey-lift-off-part4/server/src/schema.ts
+++ odyssey-lift-off-part4/server/src/schema.ts
@@ -10,6 +10,10 @@ export const typeDefs = gql`
     module(id: ID!): Module!
   }
 
+  type Mutation {
+    incrementTrackViews(id: ID!): IncrementTrackViewsResponse!
+  }
+
   "A track is a group of Modules that teaches about a specific topic"
   type Track {
     id: ID!
@@ -51,5 +55,16 @@ export const typeDefs = gql`
     content: String
     "The module's video url, for video-based modules"
     videoUrl: String
+  }
+
+  type IncrementTrackViewsResponse {
+    "Similar to HTTP status code, represents the status of the mutation"
+    code: Int!
+    "Indicates whether the mutation was successful"
+    success: Boolean!
+    "Human-readable message for the UI"
+    message: String!
+    "Newly updated track after a successful mutation"
+    track: Track
   }
 `;
